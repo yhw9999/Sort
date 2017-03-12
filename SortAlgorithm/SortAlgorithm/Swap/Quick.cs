6bd65f8c6c3d9dfb70ce44746c8387567ac70440
염희웅 <nyhw999@naver.com>
2017-03-12 13:15:26 +0900

    tmp

---
+++ SortAlgorithm/SortAlgorithm/Swap/Quick.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortAlgorithm.Swap
+{
+    class Quick : Sorter, ISort
+    {
+        public void Sort(int[] array)
+        {
+            int frontPrimary = array[0];
+            int backPrimary = array[array.Length];
+
+
+            
+        }
+    }
+}
